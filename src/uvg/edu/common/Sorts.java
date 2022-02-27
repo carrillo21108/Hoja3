0656cf2f400dd6014c8aa83978684a3665bdecd2
lop21666 <91706525+lop21666@users.noreply.github.com>
2022-02-26 23:32:53 -0600

    Implementación mergeSort y gnomeSort, pruebas Junit

--- src/uvg/edu/common/Sorts.java
+++ src/uvg/edu/common/Sorts.java
@@ -15,4 +15,75 @@ public class Sorts<T> {
 	public Sorts(IComparator compare) {
 		this.compare = compare;
 	}
+	
+	
+	// Gnome Sort
+	
+	public void gnomeSort( T[] myArray){
+        int i = 0;
+        while (i < myArray.length) {
+            if (i == 0) {            	
+            	i++;
+            }
+            if(compare.Compare(myArray[i], myArray[i-1]) >= 0) {
+            	i++;
+            }else {
+                T temp;
+                temp = myArray[i];
+                myArray[i] = myArray[i - 1];
+                myArray[i - 1] = temp;
+                i--;
+            }
+        }
+        return;
+    }
+	
+	
+	// Merge Sort
+	
+	public void mergeSort(T[] myArray, int length) {
+	    if (length < 2) {
+	        return;
+	    }
+	    int mid = length / 2;
+	    Integer[] l = new Integer[mid];
+	    Integer[] r = new Integer[length - mid];
+
+	    for (int i = 0; i < mid; i++) {
+	        l[i] = (Integer) myArray[i];
+	    }
+	    for (int i = mid; i < length; i++) {
+	        r[i - mid] = (Integer) myArray[i];
+	    }
+	    
+	    mergeSort((T[]) l, mid);
+	    mergeSort((T[]) r, length - mid);
+
+	    merge(myArray, (T[])l, (T[])r, mid, length - mid);
+	}
+	
+	private void merge(T[] myArray, T[] l, T[] r, int left, int right) {
+	 
+	    int i = 0, j = 0, k = 0;
+	    while (i < left && j < right) {
+	    	if(compare.Compare(l[i], r[j]) >= -1) {
+	    		myArray[k++] = l[i++];
+	    	}else {
+	    		myArray[k++] = r[j++];
+	        }
+	    }
+	    while (i < left) {
+	    	myArray[k++] = l[i++];
+	    }
+	    while (j < right) {
+	    	myArray[k++] = r[j++];
+	    }
+	} 
+	
+	
+	//Radix Sort
+	
+	
+	
+	
 }
--- test/uvg/edu/test/SortsTest.java
+++ test/uvg/edu/test/SortsTest.java
@@ -7,6 +7,9 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import uvg.edu.common.IntegerComparator;
+import uvg.edu.common.Sorts;
+
 /**
  * @author Brian Carrillo
  *
@@ -16,20 +19,49 @@ class SortsTest {
 	@Test
 	void GnomeSortTest() {
 		
+		Sorts sorts = new Sorts(new IntegerComparator());
+		
+		Integer[] myArray = new Integer[4];
+		myArray[0] = 4;
+		myArray[1] = 2;
+		myArray[2] = 1;
+		myArray[3] = 3;
+		
+		sorts.gnomeSort(myArray); 
+		
+		assertEquals(myArray[0], 1);
+		assertEquals(myArray[1], 2);
+		assertEquals(myArray[2], 3);
+		assertEquals(myArray[3], 4);
+		
 	}
 	
 	@Test
 	void MergeSortTest() {
 		
-	}
-	
-	@Test
-	void QuickSortTest() {
+		Sorts sorts = new Sorts(new IntegerComparator());
+		Integer[] myArray = new Integer[4];
+		myArray[0] = 4;
+		myArray[1] = 2;
+		myArray[2] = 1;
+		myArray[3] = 3; 
+		
+		sorts.gnomeSort(myArray);
+		
+		assertEquals(myArray[0], 1);
+		assertEquals(myArray[1], 2);
+		assertEquals(myArray[2], 3);
+		assertEquals(myArray[3], 4);
 		
 	}
 	
 	@Test
 	void RadixSortTest() {
+		
+	}
+	
+	@Test
+	void QuickSortTest() {
 		
 	}
 	
