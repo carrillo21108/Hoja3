1e84b3f9a7764c60a7eda046ff121c0d17d9e780
wachuuu <88355602+Wachuuu15@users.noreply.github.com>
2022-03-01 20:06:41 -0600

    implementación de quicksort y bubblesort, pruebas Junit

--- test/uvg/edu/test/SortsTest.java
+++ test/uvg/edu/test/SortsTest.java
@@ -94,11 +94,35 @@ class SortsTest {
 	
 	@Test
 	void QuickSortTest() {
+		Sorts sorts = new Sorts(new IntegerComparator());
+		Integer[] myArray = new Integer[4];
+		myArray[0] = 4;
+		myArray[1] = 2;
+		myArray[2] = 6;
+		myArray[3] = 1;
 		
+		sorts.QuickSort(myArray, 0, myArray.length -1); 
+		
+		assertEquals(myArray[0], 1);
+		assertEquals(myArray[1], 2);
+		assertEquals(myArray[2], 4);
+		assertEquals(myArray[3], 6);
 	}
 	
 	@Test
 	void BubbleSortTest() {
+		Sorts sorts = new Sorts(new IntegerComparator());
+		Integer[] myArray = new Integer[4];
+		myArray[0] = 1;
+		myArray[1] = 7;
+		myArray[2] = 2;
+		myArray[3] = 3;
+		
+		sorts.BubbleSort(myArray);
+		assertEquals(myArray[0], 1);
+		assertEquals(myArray[1], 2);
+		assertEquals(myArray[2], 3);
+		assertEquals(myArray[3], 7);
 		
 	}
 
